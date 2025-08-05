fb6e579d79af5afbef3eaf8b7533a13efd811038
furknsyl <fsoylu500@gmail.com>
2025-08-05 15:35:37 +0300

    Add project files.

---
+++ ApiProject.WebApi/Context/ApiContext.cs
@@ -0,0 +1,26 @@
+﻿using ApiProject.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProject.WebApi.Context
+{
+    public class ApiContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-CFA56CN;initial catalog=ApiYummyDb;integrated security=true;TrustServerCertificate=True;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<YummyEvent> YummyEvents { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<About> Abouts { get; set; }
+    }
+}
