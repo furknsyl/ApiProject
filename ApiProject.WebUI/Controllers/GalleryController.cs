f39c6db2a35f4f2999c801429ba6c3dc6ea09d67
furknsyl <fsoylu500@gmail.com>
2025-08-14 13:38:36 +0300

    OpenAI included

---
+++ ApiProject.WebUI/Controllers/GalleryController.cs
@@ -0,0 +1,27 @@
+﻿using ApiProject.WebUI.Dtos.ImageDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProject.WebUI.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public GalleryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> ImageList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7189/api/Images");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultImageDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
